d2e2dd23e8d2d0a8bea0a3eba3cd565a302e1823
Comayocode <ronalc726@gmail.com>
2023-07-10 18:07:32 -0500

    Add intro section

---
+++ 02.Introduccion/assets/js/app.js
@@ -0,0 +1,24 @@
+let a=10,
+  b=20,
+  c=30,
+  d='Hola ',
+  e='Manuel',
+  x=a+b;
+
+const saludo = d + e;
+
+
+
+d = 'Hola de nuevo';
+// Dar estilos a palabras en consola
+// console.log('%c Mis variables', 'color: red; font-weight: bold;');
+// console.log({a,b,c,x});
+
+// Imprimir una tabla con las variables como un objeto
+// console.table({ a,b,c,x });
+
+
+// --- ERROR AL USAR VAR COMO INCIALIZACION ---
+// var outerHeight = 10000;
+// console.log(miNombreCompleto);
+// let miNombreCompleto = "Ronald Francisco";
